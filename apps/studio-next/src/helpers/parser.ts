59e7df05f4aaa49bd0c4b7c9319bc8a77c6eedd9
Ankit Dash <ankitdash2019@gmail.com>
2024-06-10 15:51:18 +0530

    take substring if document is too long

--- apps/studio-next/src/helpers/parser.ts
+++ apps/studio-next/src/helpers/parser.ts
@@ -3,49 +3,41 @@ import { DocumentInfo } from '@/types';
 
 export default async function parseURL(base64Document: string): Promise<DocumentInfo> {
     const parser = new Parser();
-    const decodedDocument = Buffer.from(base64Document, "base64").toString("utf-8");
+    let decodedDocument = '';
+
+    if (base64Document.length >= 300) {
+        decodedDocument = Buffer.from(base64Document.substring(0, 300), "base64").toString("utf-8");
+    }
+    decodedDocument = Buffer.from(base64Document.substring(0, 300), "base64").toString("utf-8");
+    const { document, diagnostics } = await parser.parse(decodedDocument);
+
+    if (diagnostics) {
+        throw new Error('Document is not a valid AsyncAPI document');
+    }
+
+    let title = document?.info().title();
+    if (title !== undefined) {
+        title = title.length <= 20 ? title : title.slice(0, 20) + "...";
+    }
+    const version = document?.info().version();
+
+    let description = document?.info().description();
+    if (description !== undefined) {
+        description = description.length <= 100 ? description : description.slice(0, 100) + "...";
+    }
+
+    const servers = document?.allServers();
+    const channels = document?.allChannels();
     
-    const { document } = await parser.parse(decodedDocument);
+    const numServers = servers?.length;
+    const numChannels = channels?.length;
 
-    const extractInfo = async () => {
-        const info = document?.info();
-        
-        let title = info?.title();
-        if (title) {
-            title = title.length <= 20 ? title : title.slice(0, 20) + "...";
-        }
-        
-        const version = info?.version();
-        
-        let description = info?.description();
-        if (description) {
-            description = description.length <= 100 ? description : description.slice(0, 100) + "...";
-        }
-
-        return { title, version, description };
-    };
-
-    async function extractServers() {
-        const servers = document?.allServers();
-        const numServers = servers ? Object.keys(servers).length : 0;
-        return numServers;
-    };
-
-    async function extractChannels() {
-        const channels = document?.allChannels();
-        const numChannels = channels ? Object.keys(channels).length : 0;
-        return numChannels;
-    };
-
-    const [info, numServers, numChannels] = await Promise.all([
-        extractInfo(),
-        extractServers(),
-        extractChannels()
-    ]);
-
-    return {
-        ...info,
+    const response = {
+        title,
+        version,
+        description,
         numServers,
         numChannels
     };
+    return response;
 }
