4c15ff53244ad4231d4f0696cdf3bc573a6d7357
Ankit Dash <ankitdash2019@gmail.com>
2024-07-30 14:59:44 +0530

    add query params only

--- apps/studio-next/src/middleware.ts
+++ apps/studio-next/src/middleware.ts
@@ -26,5 +26,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/:base64', '/:url'],
+  matcher: [
+    '/\\?base64=:base64',
+    '/\\?url=:url'
+  ]
 }
