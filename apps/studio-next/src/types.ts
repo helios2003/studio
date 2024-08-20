8aa178e3730b6a8865c50caab1e9e0b627881970
Ankit Dash <ankitdash2019@gmail.com>
2024-08-20 19:05:45 +0530

    keep only the tests

--- apps/studio-next/src/types.ts
+++ apps/studio-next/src/types.ts
@@ -2,12 +2,3 @@ import type specs from '@asyncapi/specs';
 
 export type SpecVersions = keyof typeof specs.schemas;
 
-export interface DocumentInfo  {
-    title? : string,
-    version? : string,
-    description? : string,
-    numServers? : number,
-    numChannels? : number,
-    numOperations? : number,
-    numMessages?: number
-}
